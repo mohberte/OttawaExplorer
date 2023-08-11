504ab819348dad7e9bdbc8f5a99b9ff433ee3193
Mohamed Berte <flegpin@yahoo.fr>
2023-08-11 16:14:56 -0400

    commit

--- src/components/PictureCarousel.jsx
+++ src/components/PictureCarousel.jsx
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 import picture1 from "../assets/ottawa1.jpg";
 import picture2 from "../assets/parliament1.jpg";
 import picture3 from "../assets/guard1.jpg";
@@ -36,12 +33,3 @@ class PictureCarousel extends Component {
 };
 
 export default PictureCarousel;
-//ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
-
-// Don't forget to include the css in your page
-
-// Using webpack or parcel with a style loader
-// import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
-
-// Using html tag:
-// <link rel="stylesheet" href="<NODE_MODULES_FOLDER>/react-responsive-carousel/lib/styles/carousel.min.css"/>
